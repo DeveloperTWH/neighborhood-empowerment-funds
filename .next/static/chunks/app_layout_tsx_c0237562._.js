bd72d2eb384ce8c276dd29ffea2dc90b8e5a332d
DeveloperTWH <webdevelopment.twh@gmail.com>
2025-06-02 18:26:32 +0530

    building the animation to deploy

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules_bbb103d0._.js",
-  "static/chunks/app_components_8bfa12f1._.js"
-],
-    source: "dynamic"
-});
